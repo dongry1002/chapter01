5832438e130e20962752b99e97369efb6f577051
T3AD20 <T3AD20@G209-16>
2017-01-05 11:20:09 +0900

    parseInteger for(:)

---
+++ src/chapter01/ArrayTest02.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class ArrayTest02 {
+
+	public static void main(String[] args) {
+		//배열의 선언
+		int[] intArray1; //null이들어감
+		
+		
+		int[] intArray = new int [4];     //추천
+		int   intArray2[]= new int [3];
+
+	}
+
+}
---
+++ src/chapter01/SumToInput02.java
@@ -0,0 +1,31 @@
+package chapter01;
+
+public class SumToInput02 {
+
+	public static void main(String[] args) {
+		//스캐너 말고 args에서 가져오기  java SumToInput 10 엔터
+		if(args.length==0){
+			System.out.println("usage: java SumToInput02 [number]");
+			return;
+		}
+		///////// System.out.println(args[0]);//10은 String
+		
+		
+		int number=Integer.parseInt(args[0]);
+		
+		int sum=0;
+		for(int i=1; i<=number;i++){
+			sum+=i;
+			
+		}
+		
+		System.out.println(sum);
+		
+		}
+		
+		
+		
+	
+	
+	
+}
